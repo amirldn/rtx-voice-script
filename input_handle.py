45213df15bcda2061a27ce7a27f9751253eea55d
Amir Maula <62271191+amirmaula@users.noreply.github.com>
2020-05-08 20:06:00 +0100

    Update: Implemented arguments for selecting input file and output file, tidied implementation

--- input_handle.py
+++ input_handle.py
@@ -1,5 +1,9 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
 from pygame._sdl2 import get_num_audio_devices, get_audio_device_name
+import sys
+import getopt
 
 
 def convert(seconds):
@@ -7,12 +11,20 @@ def convert(seconds):
     seconds %= 3600
     mins = seconds // 60
     seconds %= 60
+    if len(str(hours)) == 1:
+        hours = "0"+str(hours)
+    if len(str(mins)) == 1:
+        mins = "0"+str(mins)
+    if len(str(seconds)) == 1:
+        seconds = "0"+str(seconds)
     return hours, mins, seconds
 
 
 def select_speaker_output():
     mixer.init()
-    speakerDevices = [get_audio_device_name(x, 0).decode() for x in range(get_num_audio_devices(0))]
+    speakerDevices = [
+        get_audio_device_name(x, 0).decode() for x in range(get_num_audio_devices(0))
+    ]
     mixer.quit()
     # print(speakerDevices)
     for i in speakerDevices:
@@ -28,12 +40,39 @@ def select_speaker_output():
     for i in speakerDevices:
         print(str(loop_count) + ".", i)
         loop_count += 1
-
     print("\nIf VB Audio is not found here, please check it is not disabled.")
     speaker_choice = input("\nPlease select the VB Audio speaker input: ")
     return speakerDevices[int(speaker_choice)]
 
     return None
 
+def song_select():
+    import easygui
+    selected_file=easygui.fileopenbox()
+    print(selected_file)
+    return selected_file
 
-
+def arguments(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print ('rtx-core.py -i <file input path> -o <file name for output>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('rtx-core.py -i <file input path> -o <file name for output>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    if outputfile == '':
+        outputfile = "rtx-export"
+    if inputfile == '':
+        print ('rtx-core.py -i <file input path> -o <file name for output>')
+        sys.exit(2)
+    # print ('Input file is', inputfile)
+    # print ('Output file is', outputfile)
+    return inputfile,outputfile
--- record_handle.py
+++ record_handle.py
@@ -1,53 +1,86 @@
-import pyaudio
 import wave
+
+import pyaudio
+
 
 def choose_rtx_output():
     pyaudio_rtx_instance = pyaudio.PyAudio()
-
     info = pyaudio_rtx_instance.get_host_api_info_by_index(0)
-    numdevices = info.get('deviceCount')
+    numdevices = info.get("deviceCount")
     for i in range(0, numdevices):
-        if (pyaudio_rtx_instance.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            device = pyaudio_rtx_instance.get_device_info_by_host_api_device_index(0, i).get('name')
+        if (
+            pyaudio_rtx_instance.get_device_info_by_host_api_device_index(0, i).get(
+                "maxInputChannels"
+            )
+        ) > 0:
+            device = pyaudio_rtx_instance.get_device_info_by_host_api_device_index(
+                0, i
+            ).get("name")
             # print("Input ID ", i, " - ", device)
             if "NVIDIA RTX Voice" in device:
-                pyaudio_rtx_instance.terminate()
-                return i
+                print("\nUsing " + device)
+                valid = input("Is this correct (y/n)?  ")
+                if valid.lower() == "y":
+                    pyaudio_rtx_instance.terminate()
+                    return i
+
+    # If RTX not found then
+    print("\n Could not find NVIDIA RTX Microphone.\n")
+    print("Audio Options: ")
+    for i in range(0, numdevices):
+        if (
+            pyaudio_rtx_instance.get_device_info_by_host_api_device_index(0, i).get(
+                "maxInputChannels"
+            )
+        ) > 0:
+            device = pyaudio_rtx_instance.get_device_info_by_host_api_device_index(
+                0, i
+            ).get("name")
+            print( str(i)+ ".", device)
+
+    print("\nIf RTX Microphone is not found here, please check it is not disabled.")
+    microphone_choice = input("\nPlease select the RTX Microphone input to record: ")
+    return int(microphone_choice)
 
 
-
-def record(length=10,song_name="file"):
+def record(length=10, user_output_name="file", mic_input=''):
     FORMAT = pyaudio.paInt16
     CHANNELS = 2
     RATE = 44100
     CHUNK = 1024
     RECORD_SECONDS = length
-    WAVE_OUTPUT_FILENAME = song_name+".wav"
+    WAVE_OUTPUT_FILENAME = user_output_name + ".wav"
 
-    pyaudio_instance = pyaudio.PyAudio()
+    pyaudio_inst = pyaudio.PyAudio()
 
     # start Recording
-    stream = pyaudio_instance.open(format=FORMAT, channels=CHANNELS, input_device_index = choose_rtx_output(),
-                        rate=RATE, input=True,
-                        frames_per_buffer=CHUNK)
-    print ("recording...")
+    stream = pyaudio_inst.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        input_device_index=mic_input,
+        rate=RATE,
+        input=True,
+        frames_per_buffer=CHUNK,
+    )
+    print("Recording to memory...")
 
     frames = []
 
     for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
         frames.append(data)
-    print ("finished recording")
+    print("Song complete, writing to disk...")
 
-    # stop Recording
+    # Stop recording
     stream.stop_stream()
     stream.close()
-    pyaudio_instance.terminate()
+    pyaudio_inst.terminate()
 
-    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, "wb")
     waveFile.setnchannels(CHANNELS)
-    waveFile.setsampwidth(pyaudio_instance.get_sample_size(FORMAT))
+    waveFile.setsampwidth(pyaudio_inst.get_sample_size(FORMAT))
     waveFile.setframerate(RATE)
-    waveFile.writeframes(b''.join(frames))
+    waveFile.writeframes(b"".join(frames))
     waveFile.close()
+    print("Written to " + WAVE_OUTPUT_FILENAME)
     return None
